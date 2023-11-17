3a2ecd5b9e54646828fec5523b55d530496e0dec
DionSat <43707506+DionSat@users.noreply.github.com>
2023-11-17 14:06:08 -0800

    Added basic ping command for testing

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 require('dotenv').config()
-const {Client, Events, GatewayIntentBits } = require('discord.js');
+const {Client, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
 client.once(Events.ClientReady, c => {
     console.log(`Logged in as ${c.user.tag}`)
+
+    const ping = new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with "Pong!"')
+
+    client.application.commands.create(ping, "518309568632193036");
 })
+
+client.on(Events.InteractionCreate, interaction => {
+    if(!interaction.isChatInputCommand()) return;
+    if(interaction.commandName === "ping") {
+        interaction.reply("Pong!");
+    }
+    console.log(interaction);
+});
 
 client.login(process.env.DISCORD_TOKEN)

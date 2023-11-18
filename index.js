230cdda069ba7eadf255b882509e001932cd056c
DionSat <43707506+DionSat@users.noreply.github.com>
2023-11-18 13:36:16 -0800

    Added hello command to practice

--- index.js
+++ index.js
@@ -6,17 +6,26 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 client.once(Events.ClientReady, c => {
     console.log(`Logged in as ${c.user.tag}`)
 
-    const ping = new SlashCommandBuilder()
+    const hello = new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with "Pong!"')
 
+    const ping = new SlashCommandBuilder()
+        .setName('hello')
+        .setDescription('Says hello to someone"')
+
     client.application.commands.create(ping, "518309568632193036");
+    client.application.commands.create(hello, "518309568632193036");
 })
 
 client.on(Events.InteractionCreate, interaction => {
     if(!interaction.isChatInputCommand()) return;
     if(interaction.commandName === "ping") {
         interaction.reply("Pong!");
+    }
+
+    if(interaction.commandName === "hello") {
+        interaction.reply(`Hello ${interaction.user.username}`);
     }
     console.log(interaction);
 });

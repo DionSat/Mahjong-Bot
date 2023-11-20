1cd2d1eb510be1b13d1fddb2646d1897d7cf52e6
DionSat <43707506+DionSat@users.noreply.github.com>
2023-11-20 13:50:40 -0800

    Added function to get the commands for the index.js

--- index.js
+++ index.js
@@ -25,3 +25,33 @@ client.on(Events.InteractionCreate, (interaction) => {
 });
 
 client.login(process.env.DISCORD_TOKEN)
+
+function getCommands(dir) {
+    let commands = new Collection();
+    const commandFiles = getFiles(dir);
+
+    for(const commandFile of commandFiles) {
+        const command = require(commandFile);
+        commands.set(command.data.toJSON().name, command);
+    }
+    return commands;
+}
+
+function getFiles(dir) {
+    const files = fs.readdirSync(dir, {
+        withFileTypes: true
+    })
+    let commandFiles = [];
+
+    for(const file of files) {
+        if(file.isDirectory()) {
+            commandFiles = [
+                ...commandFiles,
+                ...getFiles(`${dir}/${file.name}`),
+            ]
+        } else if (file.name.endsWith(".js")) {
+            commandFiles.push(`${dir}/${file.name}`);
+        }
+    }
+    return commandFiles;
+}

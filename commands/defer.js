afac5f1fd57ce1cbfa90be6e795d24b5ede22106
DionSat <43707506+DionSat@users.noreply.github.com>
2023-11-24 14:08:32 -0800

    Finished exploring the slash command options.

--- commands/defer.js
+++ commands/defer.js
@@ -1,14 +1,36 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js')
 
 module.exports = {
         data: new SlashCommandBuilder()
         .setName('defer')
-        .setDescription('Defer reply example"'),
+        .setDescription('Defer reply example"')
+        .addStringOption(option => 
+            option
+                .setName('string')
+                .setDescription('test')
+                .setMaxLength(10)
+                .setMinLength(3)
+                .setRequired(true)
+                .setChoices(
+                    {name: 'hello', value: 'hello'}, 
+                    {name: 'hii', value: 'hii'}
+                )
+        )
+        .addNumberOption(option => 
+            option
+                .setName('number')
+                .setDescription('test')
+                .setMaxValue(10)
+                .setMinValue(3)
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .setDMPermission(false),
 
         async execute(interaction) {
                 await interaction.deferReply({ephemeral: true});
                 await interaction.editReply({content: "Hello"});
 
-                await interaction.followUp({content: "Hello again!"});
+                // await interaction.followUp({content: "Hello again!"});
         }
 }

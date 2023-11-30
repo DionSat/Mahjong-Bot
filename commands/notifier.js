8a312275bcaf649f367e9bb9473aa679adc154e6
DionSat <43707506+DionSat@users.noreply.github.com>
2023-11-30 13:19:38 -0800

    added switch case for add subcommand

--- commands/notifier.js
+++ commands/notifier.js
@@ -35,41 +35,48 @@ module.exports = {
         async execute(interaction) {
                 const { options } = interaction;
                 const sub = options.getSubcommand();
-                const query = videoData.title.toLowerCase()
-                const channelID = videoData.channelID
-                await interaction.deferReply({ephemeral: true});
 
-                const url = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelID}`;
-                console.log(query);
-                console.log(channelID);
-                console.log(url);
+                switch(sub) {
+                    case 'add':
+                        if(videoData) {
+                            const query = videoData.title.toLowerCase()
+                            const channelID = videoData.channelID
+     
+                            await interaction.deferReply({ephemeral: true});
+                            const url = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelID}`;
+                            console.log(query);
+                            console.log(channelID);
+                            console.log(url);
 
-                let data = await parser.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${channelID}`)
-                let { author } = data.items[0]
+                            let data = await parser.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${channelID}`)
+                            let { author } = data.items[0]
 
-                let link = ""
-                let videoTitle = ""
+                            let link = ""
+                            let videoTitle = ""
 
-                await data.items.forEach(async value => {
-                    const title = value.title.toLowerCase();
+                            await data.items.forEach(async value => {
+                                const title = value.title.toLowerCase();
 
-                    if (title.includes(query)) {
-                        link = value.link;
-                        videoTitle = value.title
-                    } else {
-                        return;
-                    }
-                });
-                console.log(link);
-                console.log(videoTitle);
+                                if (title.includes(query)) {
+                                    link = value.link;
+                                    videoTitle = value.title
+                                } else {
+                                    return;
+                                }
+                            });
+                            console.log(link);
+                            console.log(videoTitle);
 
-                if (link.length === 0 || videoTitle.length === 0) {
-                    return await interaction.editReply({ content: `There are no **recent** videos matching \`${query}\` on  ${author}'s channel!`, ephemeral: true })
-                }
-                try {
-                    await interaction.editReply({ content: `**Video Matching** \`${query}\` \n \n\`\`\`${videoTitle}\`\`\` \n \n \n> ${link} \n \n*Please note, not all links will be embedded*`, ephemeral: true})
-                } catch(e) {
-                    return await interaction.editReply({ content: `There are **SO MANY** video matching ${query} that I cant send a message with them`, ephemeral: true})
+                            if (link.length === 0 || videoTitle.length === 0) {
+                                return await interaction.editReply({ content: `There are no **recent** videos matching \`${query}\` on  ${author}'s channel!`, ephemeral: true })
+                            }
+                            try {
+                                await interaction.editReply({ content: `**Video Matching** \`${query}\` \n \n\`\`\`${videoTitle}\`\`\` \n \n \n> ${link} \n \n*Please note, not all links will be embedded*`, ephemeral: true})
+                            } catch(e) {
+                                return await interaction.editReply({ content: `There are **SO MANY** video matching ${query} that I cant send a message with them`, ephemeral: true})
+                            }
+                        }
+
                 }
         }
 }

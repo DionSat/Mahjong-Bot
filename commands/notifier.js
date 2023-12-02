723e5e36922fc00356f2b196349c2600c734225f
DionSat <43707506+DionSat@users.noreply.github.com>
2023-12-02 13:11:45 -0800

    Got rid of some of the unecessary info

--- commands/notifier.js
+++ commands/notifier.js
@@ -10,44 +10,43 @@ module.exports = {
         
 
         async execute(interaction) {
-                const { options } = interaction;
-                const sub = options.getSubcommand();
-
-                const ID = options.getString('channel-id')
-                const title = options.getString('title')
-                if(videoData.title === title && videoData.episode !== episode) {
-                    const query = title
+                if(videoData.title) {
+                    const query = title;
 
                     await interaction.deferReply({ephemeral: true});
                     const url = `https://www.youtube.com/feeds/videos.xml?channel_id=${ID}`;
 
-                    let data = await parser.parseURL(url)
-                    let { author } = data.items[0]
+                    let data = await parser.parseURL(url);
 
-                    let link = ""
-                    let videoTitle = ""
+                    let link = "";
+                    let videoTitle = "";
+                    let maxEpisode = 0;
 
                     await data.items.forEach(async value => {
                         const title = value.title.toLowerCase();
 
                         if (title.includes(query)) {
+                            episode = title.match(/(\d+)/);
+                            maxEpisode = Math.max(maxEpisode, episode);
                             link = value.link;
-                            videoTitle = value.title
+                            videoTitle = value.title;
                         } else {
                             return;
                         }
                     });
-                    console.log(link);
-                    console.log(videoTitle);
+                    episode = videoTitle.match(/(\d+)/);
 
                     if (link.length === 0 || videoTitle.length === 0) {
-                        return await interaction.editReply({ content: `There are no **recent** videos matching \`${query}\` on  ${author}'s channel!`, ephemeral: true })
+                        // return await interaction.editReply({ content: `There are no **recent** videos matching \`${query}\` on  ${author}'s channel!`, ephemeral: true })
+                        return await interaction.deleteReply();
+                    } else {
+                        await interaction.editReply({ content: `**Video Matching** \`${query}\` \n \n\`\`\`${videoTitle}\`\`\` \n \n \n> ${link} \n \n*Please note, not all links will be embedded*`, ephemeral: true});
                     }
-                    try {
-                        await interaction.editReply({ content: `**Video Matching** \`${query}\` \n \n\`\`\`${videoTitle}\`\`\` \n \n \n> ${link} \n \n*Please note, not all links will be embedded*`, ephemeral: true})
-                    } catch(e) {
-                        return await interaction.editReply({ content: `There are **SO MANY** video matching ${query} that I cant send a message with them`, ephemeral: true})
-                    }
+                    // try {
+                    //     await interaction.editReply({ content: `**Video Matching** \`${query}\` \n \n\`\`\`${videoTitle}\`\`\` \n \n \n> ${link} \n \n*Please note, not all links will be embedded*`, ephemeral: true})
+                    // } catch(e) {
+                    //     return await interaction.editReply({ content: `There are **SO MANY** video matching ${query} that I cant send a message with them`, ephemeral: true})
+                    // }
                 } else {
                     return
                 }

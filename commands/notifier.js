5a95f572fb91813335f41567ebb1536d25f025c3
DionSat <43707506+DionSat@users.noreply.github.com>
2023-12-04 14:08:32 -0800

    Added back notifier subcommand add and remove so that you can enable and disable the notifier

--- commands/notifier.js
+++ commands/notifier.js
@@ -7,8 +7,44 @@ const fs = require('fs');
 module.exports = {
         data: new SlashCommandBuilder()
         .setName('notifier')
-        .setDescription('Notify discord channel of new videos"'),
+        .setDescription('Notify discord channel of new videos"')
+        .addSubcommand(command =>
+                command
+                    .setName('add')
+                    .setDescription('Set up youtube notification system')
+                    .addStringOption(option => 
+                        option
+                            .setName('channel-id')
+                            .setDescription('The user to repeat')
+                            .setRequired(true)
+                    )
+                    .addChannelOption(option => 
+                        option
+                            .setName('channel')
+                            .setDescription('The channel to set the notification in')
+                            .setRequired(true)
+                    )
+                    .addStringOption(option => 
+                        option
+                            .setName('title')
+                            .setDescription('Title of the video')
+                            .setRequired(true)
+                    )
+        )
+        .addSubcommand(command =>
+                command
+                    .setName('remove')
+                    .setDescription('Set up youtube notification system')
+                    .addStringOption(option => 
+                        option
+                            .setName('title')
+                            .setDescription('Title of the video')
+                            .setRequired(true)
+                    )
+        ),
         
 
-        async execute(interaction) {}
+        async execute(interaction) {
+                
+        }
 }

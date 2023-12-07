66755e02e86855bf795b69fabf6be845d6dbdf33
DionSat <43707506+DionSat@users.noreply.github.com>
2023-12-07 12:49:55 -0800

    Changed the YT check to every one hour

--- events/ready.js
+++ events/ready.js
@@ -11,7 +11,7 @@ module.exports = {
             console.log('checking...');
             setIntervalAsync(async () => {
                 await checkUpdates(client)
-              }, 30000);
+              }, 3600000);
         }
     }
 }

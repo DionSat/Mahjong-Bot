92160e33f1ab2cdcea02a50cc505b0ffd7f9a609
DionSat <43707506+DionSat@users.noreply.github.com>
2023-12-12 08:05:06 -0800

    Trying to make system of representing the tiles suites as numbers for better processing

--- functions/efficiency.js
+++ functions/efficiency.js
@@ -1,3 +1,26 @@
-module.exports = async interaction => {
+module.exports = async (interaction, hand) => {
     
+}
+
+function parseHand(hand) {
+    tiles = []
+    let j = 0
+    for(let i = 0; i < hand.length; i++) {
+        if(hand[i] === 'g') {
+            tiles[j] = 30 + hand[i - 1]
+            j++
+        }
+        if(hand[i] === 'm') {
+            tiles[j] = 10 + hand[i - 1]
+            j++
+        }
+        if(hand[i] === 'p') {
+            tiles[j] = 20 + hand[i - 1]
+            j++
+        }
+        if(hand[i] === 's') {
+            tiles[j] = 30 + hand[i - 1]
+            j++
+        }
+    }
 }

cd207f1d5cbd7301a93c8da066a3acdf8e68b318
DionSat <43707506+DionSat@users.noreply.github.com>
2023-12-14 10:11:48 -0800

    Added a warning if numbers 8-9 are used for the honor tiles as they dont exist. Also added some style consistencies.

--- functions/efficiency.js
+++ functions/efficiency.js
@@ -2,36 +2,42 @@ module.exports = async (interaction, hand) => {
     
 }
 
-function parseHand(hand) {
-    tiles = {}
-    tileNumbers = []
-    let j = 0
+async function parseHand(hand) {
+    tiles = {};
+    tileNumbers = [];
+    let j = 0;
+    
+    let channel = await client.channels.cache.get(process.env.CHANNEL_ID);
+    if (!channel) return;
+
     for(let i = 0; i < hand.length; i++) {
         tileNumbers.push(hand[i])
         if(hand[i] === 'r') {
-            tiles['red'] = tileNumbers
-            tileNumbers = []
+            tiles['red'] = tileNumbers;
+            tileNumbers = [];
         }
         if(hand[i] === 'm') {
-            tiles['man'] = tileNumbers
-            tileNumbers = []
+            tiles['man'] = tileNumbers;
+            tileNumbers = [];
         }
         if(hand[i] === 'p') {
-            tiles['pin'] = tileNumbers
-            tileNumbers = []
+            tiles['pin'] = tileNumbers;
+            tileNumbers = [];
         }
         if(hand[i] === 's') {
-            tiles['sou'] = tileNumbers
-            tileNumbers = []
+            tiles['sou'] = tileNumbers;
+            tileNumbers = [];
         }        
         if(hand[i] === 'h') {
             if(tileNumbers.includes('8') || tileNumbers.includes('9')) {
-                
+                await channel.send({ content: `Warning honor tiles are not valid. Has to be a number 1 - 7 to represent east, west, north, south, green, white, red respectively.`, ephemeral: true });
             }
             else {
-                tiles['honor'] = tileNumbers
-                tileNumbers = []
+                tiles['honor'] = tileNumbers;
+                tileNumbers = [];
             }
         }
     }
+
+    return tileNumbers;
 }

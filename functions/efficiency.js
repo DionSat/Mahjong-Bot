be79a44b7339c7d3dce00e7876ef260d443d6c10
DionSat <43707506+DionSat@users.noreply.github.com>
2023-12-23 13:44:06 -0800

    Added binary search to look for sequences since array is sorted.

--- functions/efficiency.js
+++ functions/efficiency.js
@@ -73,29 +73,35 @@ module.exports = async (interaction, hand) => {
     console.log("Shanten: ", shantenScore);
 }
 
-async function parseSequence(hand, blocks, sets) {
-    let l = 0
-    let r = 0
+async function parseSequence(hand, blocks) {
     let seq = {}
-    while (l < hand.length) {
-        if (seq.length === 3) {
-            l += 1
-            blocks.push(seq)
-            sets += 1
-            seq = []
-            seq.push(hand[l])
-        }
-        if(l === r) {
-            seq[l] = hand[l]
-        }
-        if(hand[l] + 1 === hand[r]) {
-            seq[r] = hand[r]
-            seqLen -= 1
-        }
-        else {
-            r += 1
-        }
+    for(let i = 0; i < hand.length; i++) {
+        let s1 = hand[i];
+        let s2 = binarySearch(hand, hand[i] + 1, i, hand.length);
+        let s3 = binarySearch(hand, hand[i] + 2, i, hand.length);
+        if(s2 !== -1 && s3)
     }
+}
+
+function binarySearch(arr, x, start, end) {
+    // Base Condition
+    if (start > end) return -1;
+    
+    // Find the middle index
+    let mid = Math.floor((start + end) / 2);
+
+    // Compare mid with given key x
+    if (arr[mid] === x) return arr[mid];
+
+    // If element at mid is greater than x,
+    // search in the left half of mid
+    if (arr[mid] > x)
+        return recursiveFunction(arr, x, start, mid - 1);
+    else
+
+        // If element at mid is smaller than x,
+        // search in the right half of mid
+        return recursiveFunction(arr, x, mid + 1, end);
 }
 
 async function parseHand(hand, interaction) {
